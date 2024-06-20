2c5f1ad9e43a397223aff9a3a117f8f1c8b012c2
LengBTan <letng27@gmail.com>
2024-06-20 13:56:01 -0400

    Add logger to console

--- src/main/java/org/shulkerdeposit/shulkerdeposit/ShulkerDeposit.java
+++ src/main/java/org/shulkerdeposit/shulkerdeposit/ShulkerDeposit.java
@@ -7,11 +7,12 @@ public final class ShulkerDeposit extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("ShulkerDeposit on");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("ShulkerDeposit off");
     }
 }
